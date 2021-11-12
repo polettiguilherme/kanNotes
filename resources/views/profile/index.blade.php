bed520e9c21283af8c537d7719bc4ad6c7f9cacf
Guilherme Poletti <polettigui15@gmail.com>
2021-11-12 19:21:39 -0300

    tela do perfil

--- resources/views/profile/index.blade.php
+++ resources/views/profile/index.blade.php
@@ -7,15 +7,21 @@
     <div class="container-profile">    
         <div class="profile">
             <div class="picture">
-
+                <img src="{{ asset('images/eu.jpg') }}" alt="foto-de-perfil" class="picture-img">
             </div>
             <div class="name">
-            
+                <p class="label">Nome</p> 
+                {{ @session('email.name') }}
             </div>
             <div class="user">
-            
+                <p class="label">Usuário</p> 
+                {{ @session('email.user') }}
+            </div>
+            <div class="edit">
+                    <button class="btn "> <i class="bi bi-pencil-fill"></i> Editar Perfil  </button>
             </div>
         </div>
+        
     </div>
 @endsection  
 
